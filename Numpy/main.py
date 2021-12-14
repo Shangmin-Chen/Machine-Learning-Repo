82c0d3df86e954b1762734fe516ba426c2e2e3d8
Shangmin-Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2021-12-14 11:38:41 -0500

    Update main.py

--- Numpy/main.py
+++ Numpy/main.py
@@ -53,6 +53,97 @@ print(a[1,5])
 # get a specific row
 print(a[0,:])
 
+# get a specific column
+print(a[:,2])
 
+# [start_index:end_index:step_size]
+print(a[0, 1:6:2])
 
+# you can change these index aswell
 
+# Initializing different types of arrays
+
+# all 0s matrix
+print()
+print(np.zeros((2,3)))
+
+# all 1s matrix
+print()
+print(np.ones((4,2,2), dtype="int32"))
+
+# any other number
+print()
+print(np.full((2,2),99))
+
+# full like
+print()
+print(np.full(a.shape, 4))
+
+# random deci
+print(np.random.rand(4,4,3))
+print()
+
+#random like
+print()
+print(np.random.random_sample(a.shape))
+
+# random int (start,end,size)
+print()
+print(np.random.randint(1, 7, size=(3,3)))
+
+# identity matrix
+print()
+print(np.identity(3))
+
+# repeating
+print()
+arr = np.array([[1,2,3]])
+r1 = np.repeat(arr,3,axis=0)
+print(r1)
+
+"""MATH"""
+print()
+print(a)
+print()
+print(a+2)
+print()
+print(np.cos(a))
+
+# linear algebra
+print("\n\n\nmulti matrix")
+
+a = np.ones((2,3))
+print(a)
+
+b = np.full((3,2),2)
+print(b)
+
+print(np.matmul(a,b))
+
+#statistics
+print("\n\n\nstatistics")
+
+stats = np.array([[1,2,3],[4,5,6]])
+
+print(np.min(stats))
+print(np.max(stats, axis=0))
+print(np.max(stats, axis=1))
+print(np.max(stats))
+print(np.sum(stats))
+
+#reorganize
+"""
+np.array().reshape(())
+np.vstack([v1,v2])
+np.hstack([h1, h2])
+"""
+
+#load data
+"""
+
+filedata = np.genfromtxt("data.txt"), delimiter=",")
+filedata = filedata.astype("int32")
+
+print(filedata)
+
+"""
